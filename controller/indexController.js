ff75d5bcd10dfd788939527bdb91d8d19abb3ca4
Isaac-del23 <bsisaac0323@gmail.com>
2025-08-08 22:43:22 -0300

    Nova Alteracao na pagina index,layout

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,7 +1,15 @@
 exports.index = (req, res) => {
     res.render("index", {title: "Titulo"})
-}
+};
 
-exports.home = (req, res) => {
-    res.render("home", {title: "Titulo"})
-}
+//exports.home = (req, res) => {
+//    res.render("home", {title: "Titulo"})
+//};
+
+exports.recursos = (req, res) => {
+    res.render("recursos", {title: "Titulo"})
+};
+
+exports.dados = (req, res) => {
+    res.render("dados", {title: "Titulo"})
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,6 +5,10 @@ var indexController = require('../controller/indexController')
 /* GET home page. */
 router.get('/', indexController.index);
 
-router.get('/home', indexController.home);
+//router.get('/home', indexController.home);
+
+router.get('/recursos', indexController.recursos);
+
+router.get('/dados', indexController.dados);
 
 module.exports = router;
